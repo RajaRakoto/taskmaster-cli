cb723cd5f7cb8a3243b03f0b5fe989dd9399d94f
raja <raja.rakoto7@gmail.com>
2025-06-16 19:08:32 +0300

    set target to bun

--- build.js
+++ build.js
@@ -7,15 +7,6 @@ await Bun.build({
 	minify: true,
 	splitting: false,
 	sourcemap: "external",
-	target: "node", // or 'bun', 'browser'
+	target: "bun",
 	plugins: [dts()],
 }).catch(console.error);
-
-// shebang process
-async function shebangProcess(filePath, shebang) {
-	const originalContent = await Bun.file(filePath).text();
-	const contentWithShebang = `${shebang}\n${originalContent}`;
-	await Bun.write(filePath, contentWithShebang);
-}
-
-shebangProcess("./dist/index.js", "#!/usr/bin/env node");
