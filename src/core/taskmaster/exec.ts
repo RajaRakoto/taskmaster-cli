af489bb802a15a31fc193ec21b49cdf0c146ac37
raja <raja.rakoto7@gmail.com>
2025-06-16 20:04:59 +0300

    fix: correct tmai init menu choices

--- src/core/taskmaster/exec.ts
+++ src/core/taskmaster/exec.ts
@@ -12,16 +12,17 @@ import { tmaiInitMenu_prompt } from "@/prompt";
 
 const tmai = new TaskMaster();
 
+/*******  be6e15c7-9970-4578-baf6-d31421004679  *******/
 export async function tmaiInitAsync() {
-	const choice = await inquirer.prompt(tmaiInitMenu_prompt);
+  const choice = await inquirer.prompt(tmaiInitMenu_prompt);
 
-	if (choice.tmInitMenu === "tmai-install") {
-		await tmai.installAsync();
-	} else if (choice.tmInitMenu === "tmai-init") {
-		await tmai.initAsync();
-	} else if (choice.tmInitMenu === "tmai-config") {
-		await tmai.configAsync();
-	}
+  if (choice.tmaiInitMenu === "tmai-install") {
+    await tmai.installAsync();
+  } else if (choice.tmaiInitMenu === "tmai-init") {
+    await tmai.initAsync();
+  } else if (choice.tmaiInitMenu === "tmai-config") {
+    await tmai.configAsync();
+  }
 
-	await restartAsync();
+  await restartAsync();
 }
