e7134aeab5a4ab5c263adc41a435c4d3cbbb79e6
raja <raja.rakoto7@gmail.com>
2025-06-16 19:19:18 +0300

    feat(taskmaster): add tmai init/install/config functionality

---
+++ src/core/taskmaster/exec.ts
@@ -0,0 +1,27 @@
+/* libs */
+import inquirer from "inquirer";
+
+/* core */
+import { TaskMaster } from "@/core/taskmaster/TaskMaster";
+import { restartAsync } from "@/core/restart";
+
+/* prompt */
+import { tmaiInitMenu_prompt } from "@/prompt";
+
+// ===============================
+
+const tmai = new TaskMaster();
+
+export async function tmInitAsync() {
+  const choice = await inquirer.prompt(tmaiInitMenu_prompt);
+
+  if (choice.tmInitMenu === "tmai-install") {
+    await tmai.installAsync();
+  } else if (choice.tmInitMenu === "tmai-init") {
+    await tmai.initAsync();
+  } else if (choice.tmInitMenu === "tmai-config") {
+    await tmai.configAsync();
+  }
+
+  await restartAsync();
+}
