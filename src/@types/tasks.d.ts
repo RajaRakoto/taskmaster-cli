2188c85d75ac8d66b2816a661fb4631b10d1892c
raja <raja.rakoto7@gmail.com>
2025-06-18 14:58:27 +0300

    chore(types): rename interface to I_Tasks

--- src/@types/tasks.d.ts
+++ src/@types/tasks.d.ts
@@ -1,45 +1,45 @@
-export interface TasksD {
-    master: Master;
+export interface I_Tasks {
+	master: Master;
 }
 
 export interface Master {
-    tasks:    Task[];
-    metadata: Metadata;
+	tasks: Task[];
+	metadata: Metadata;
 }
 
 export interface Metadata {
-    created:     Date;
-    updated:     Date;
-    description: string;
+	created: Date;
+	updated: Date;
+	description: string;
 }
 
 export interface Task {
-    id:           number;
-    title:        string;
-    description:  string;
-    details:      string;
-    testStrategy: string;
-    priority:     Priority;
-    dependencies: number[];
-    status:       Status;
-    subtasks:     Subtask[];
+	id: number;
+	title: string;
+	description: string;
+	details: string;
+	testStrategy: string;
+	priority: Priority;
+	dependencies: number[];
+	status: Status;
+	subtasks: Subtask[];
 }
 
 export enum Priority {
-    High = "high",
-    Low = "low",
-    Medium = "medium",
+	High = "high",
+	Low = "low",
+	Medium = "medium",
 }
 
 export enum Status {
-    Pending = "pending",
+	Pending = "pending",
 }
 
 export interface Subtask {
-    id:           number;
-    title:        string;
-    description:  string;
-    dependencies: number[];
-    details:      string;
-    status:       Status;
+	id: number;
+	title: string;
+	description: string;
+	dependencies: number[];
+	details: string;
+	status: Status;
 }
