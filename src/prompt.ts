fda1079d6621da2b8d573b514ed9e648f98cfd0a
raja <raja.rakoto7@gmail.com>
2025-07-28 16:00:55 +0300

    feat(ui): add CRUD hint to task management menu

--- src/prompt.ts
+++ src/prompt.ts
@@ -25,7 +25,7 @@ export const mainMenu_prompt = [
 				value: "tmai-gen",
 			},
 			{
-				name: `${emoji.get("heavy_check_mark")}  Task management`,
+				name: `${emoji.get("heavy_check_mark")}  Task management (CRUD)`,
 				value: "tmai-manage",
 			},
 			{
@@ -137,7 +137,7 @@ export const tmaiManageMenu_prompt = [
 		loop: true,
 		pageSize: 10,
 		choices: [
-			new inquirer.Separator("=== Task management ==="),
+			new inquirer.Separator("=== Task management (CRUD) ==="),
 			{
 				name: `${emoji.get("link")} List and Navigation`,
 				value: "tmai-listnav",
